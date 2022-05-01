61ec5bf5f4918a7b35ad2d17b6411d72b1f28d13
Angel <zhnganqi@gmail.com>
2022-05-01 20:50:20 +0800

    basic version

--- products.py
+++ products.py
@@ -7,3 +7,10 @@ while True:
     products.append([name,price])
 print(products)
 
+
+for p in products:
+	print(p[0],'的價格是',p[1])
+
+with open('products.txt','w')as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
